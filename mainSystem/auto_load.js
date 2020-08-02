ff991ff210e18501fcadb43a36752b02b02c6fbe
Khanh <khanhtq.b8084@st.usth.edu.vn>
2020-08-02 09:47:18 +0700

    test

---
+++ mainSystem/auto_load.js
@@ -0,0 +1,2 @@
+// sử dụng cho thư viện dùng chung để vào libs
+global['sample_test']  = require('./libs/module_abc')
---
+++ mainSystem/libs/module_abc.js
@@ -0,0 +1,8 @@
+// test module
+
+function test(){
+  console.log('from test module')
+  return "test abc"
+}
+
+module.exports = test
